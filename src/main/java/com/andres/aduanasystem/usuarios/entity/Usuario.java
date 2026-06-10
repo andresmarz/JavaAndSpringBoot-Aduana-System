ca86e269869c68b1da010bbe4a11825bcce7a406
andresmarz <andresmarquezzuleta@gmail.com>
2026-06-09 21:32:12 -0400

    Create entity Usuario on users

---
+++ src/main/java/com/andres/aduanasystem/usuarios/entity/Usuario.java
@@ -0,0 +1,27 @@
+package com.andres.aduanasystem.usuarios.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String cargo;
+}
