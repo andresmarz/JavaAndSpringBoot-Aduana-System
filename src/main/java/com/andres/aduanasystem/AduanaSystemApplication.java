9c38f0c80a8bdef1a4505d4eadd395dde63f5070
andresmarz <andresmarquezzuleta@gmail.com>
2026-06-09 21:03:31 -0400

    Initial commit

---
+++ src/main/java/com/andres/aduanasystem/AduanaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.andres.aduanasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AduanaSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AduanaSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andres/aduanasystem/AduanaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres.aduanasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AduanaSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
